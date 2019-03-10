1ec1b2d1d034249c1c50be024684d8d09ea69c40
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-03-10 13:04:40 -0700

    Use state hook

--- home/src/App.js
+++ home/src/App.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        App
-      </div>
-    );
+function App () {
+  const [userQuery, setUserQuery] = useState('');
+
+  const updateUserQuery = event => {
+    console.log('userQuery:', userQuery);
+    setUserQuery(event.target.value);
   }
+
+  const searchQuery =() => {
+    window.open(`htps://google.com/search?q=${userQuery}`, '_blank')
+  }
+
+  const handleKeyPress = event => {
+    if(event.key === 'Enter') {
+      searchQuery();
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello Mauricio</h1>
+      <div className='form'>
+        <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress}/>
+        <button onClick={searchQuery}>Search</button>
+      </div>
+
+    </div>
+  );
 }
+
 
 export default App;
