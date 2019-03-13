ebb5604465aa0b23a92d5a529e6595853a13d0b4
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-03-13 12:54:46 -0700

    gallery funcitonality

--- home/src/Gallery.js
+++ home/src/Gallery.js
@@ -1,20 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import PICTURES from './data/pictures';
 
+const SECONDS = 1000;
+const minimumDelay = 1 * SECONDS;
+
 function Gallery(){
   const [index, setIndex] = useState(0);
+  const [delay, setDelay] = useState(3 * SECONDS);
+  const [increment, setIncrement] = useState(1);
 
   useEffect( () => {
-    setInterval( () => {
+    console.log('delay', delay)
+    const interval = setInterval( () => {
       setIndex (storedIndex => {
         return (storedIndex + 1) % PICTURES.length;
       })
-    }, 3000);
-  }, []);
+    }, delay);
+
+    return () => {
+      console.log('remove last interval')
+      clearInterval(interval);
+    };
+  }, [delay]);
+
+  const updateDelay = event => {
+    const delay = Number(event.target.value) * SECONDS;
+
+    setDelay(delay < minimumDelay ? minimumDelay : delay);
+  }
 
   return (
     <div className='Gallery'>
       <img src={PICTURES[index].image} alt='gallery' />
+      <div className='multiform'>
+        <div>Gallery transition delay (seconds):
+          <input type='number' onChange={updateDelay}/>
+        </div>
+        <div> 
+          Gallery increment: 
+          <input type= 'number' onChange={updateDelay} />
+        </div>
+      </div>
     </div>
   )
 }
